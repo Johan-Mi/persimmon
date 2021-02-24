04f387c9cb8e8dc3a52edf421470ae3f55301734
Johan-Mi <johanmi@protonmail.com>
2021-02-24 10:39:04 +0100

    So many macros!

--- command/src/lib.rs
+++ command/src/lib.rs
@@ -2,30 +2,8 @@ mod error;
 pub mod subcommands;
 mod utils;
 
-use std::str::FromStr;
-use subcommands::Query;
-
 pub use error::CommandError;
 
-pub enum Command {
-    Query(Query),
-}
-
-impl FromStr for Command {
-    type Err = CommandError;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        let parts: Vec<_> = s.split_whitespace().collect();
-
-        let (subcommand, args) =
-            parts.split_first().ok_or(CommandError::UnexpectedEnd)?;
-
-        match *subcommand {
-            "query" | "q" => Query::parse(args).map(Command::Query),
-
-            _ => Err(CommandError::InvalidCommand {
-                name: s.to_string(),
-            }),
-        }
-    }
+def_commands! {
+    Query ("query", "q"),
 }
--- command/src/utils.rs
+++ command/src/utils.rs
@@ -40,3 +40,43 @@ macro_rules! def_subcommands {
         }
     }
 }
+
+#[macro_export]
+macro_rules! def_commands {
+    ($(
+        $cmdtype:ident ($($cmdname:literal),*)
+    ),*$(,)?) => {
+        pub enum Command {
+            $($cmdtype($crate::subcommands::$cmdtype)),*
+        }
+
+        def_commands!(@a $($($cmdtype $cmdname)*)*);
+    };
+
+    (@a $($cmdtype:ident $cmdname:literal)*) => {
+        impl ::std::str::FromStr for Command {
+            type Err = $crate::CommandError;
+
+            fn from_str(
+                s: &str
+            ) -> ::std::result::Result<Self, Self::Err> {
+                let parts: ::std::vec::Vec<_> = s.split_whitespace().collect();
+
+                let (command, args) = parts
+                    .split_first()
+                    .ok_or($crate::CommandError::UnexpectedEnd)?;
+
+                match *command {
+                    $($cmdname => {
+                        $crate::subcommands::$cmdtype::parse(args)
+                            .map(Command::$cmdtype)
+                    })*
+
+                    _ => Err(CommandError::InvalidCommand {
+                        name: s.to_string(),
+                    }),
+                }
+            }
+        }
+    };
+}
