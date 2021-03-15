1abc2c99853349effe3ec606b04555e1479a85b5
Johan-Mi <johanmi@protonmail.com>
2021-03-15 13:37:05 +0100

    Add text shadows

--- ui/src/widgets/text.rs
+++ ui/src/widgets/text.rs
@@ -1,6 +1,9 @@
 use crate::core::{Rect, Widget};
 use sdl2::{pixels::Color, rect::Rect as SdlRect};
 
+const TEXT_COLOR: Color = Color::BLACK;
+const SHADOW_COLOR: Color = Color::RGB(0xc0, 0xc0, 0xc0);
+
 pub struct Text {
     pub text: String,
     pub style: TextStyle,
@@ -16,14 +19,22 @@ impl Widget for Text {
 
         let surface = font
             .render(&self.text)
-            .blended_wrapped(Color::BLACK, boundry.width)
+            .blended_wrapped(TEXT_COLOR, boundry.width)
+            .unwrap();
+        let shadow_surface = font
+            .render(&self.text)
+            .blended_wrapped(SHADOW_COLOR, boundry.width)
             .unwrap();
         let (width, height) = surface.size();
 
         let texture = surface.as_texture(gfx.texture_creator).unwrap();
+        let shadow_texture =
+            shadow_surface.as_texture(gfx.texture_creator).unwrap();
 
         let dst = SdlRect::new(boundry.x, boundry.y, width, height);
+        let shadow_dst = SdlRect::new(boundry.x, boundry.y + 1, width, height);
 
+        gfx.canvas.copy(&shadow_texture, None, shadow_dst).unwrap();
         gfx.canvas.copy(&texture, None, dst).unwrap();
     }
 }
